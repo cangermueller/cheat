7951eb574352f23bb67c771cf075707044d6bcbd
Christof Angermueller <cangermueller@gmail.com>
2015-12-17 21:48:15 +0000

    m

--- R/argparse.R
+++ R/argparse.R
@@ -3,9 +3,19 @@
 library(argparse)
 
 p <- ArgumentParser(description='Description')
-p$add_argument('in_file', help='Input file')
-p$add_argument('-o', '--out_file', help='Output file')
-p$add_argument('--verbose', action='store_true', help='More detailed log messages')
+p$add_argument(
+  'in_file',
+  help='Input file')
+p$add_argument(
+  '-o', '--out_file',
+  default='./out_file.h5',
+  help='Output file')
+p$add_argument(
+  '--verbose',
+  action='store_true',
+  help='More detailed log messages')
 
 args <- p$parse_args(commandArgs(TRUE))
 print(args)
+
+# is.null(opts$foo)   // TRUE if not given
