e1d4bbaa7bdf2a2a4876f19f15512a0787eb48b5
Christof Angermueller <cangermueller@gmail.com>
2016-11-28 15:42:22 +0000

    Minor changes

---
+++ bash/script.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+check=1
+function run {
+  cmd=$@
+  echo
+  echo "#################################"
+  echo $cmd
+  echo "#################################"
+  eval $cmd
+  if [ $check -ne 0 -a $? -ne 0 ]; then
+    1>&2 echo "Command failed!"
+    exit 1
+  fi
+}
