13f91135f44bc2d77a3d093cac9f6b466828b1c4
Christof Angermueller <cangermueller@gmail.com>
2018-09-30 19:18:15 -0700

    Update configs

--- python/run.py
+++ python/run.py
@@ -10,7 +10,7 @@ import sys
 import argparse
 import logging
 
-#  __dir = os.path.dirname(os.path.realpath(__file__))
+# __dir = os.path.dirname(os.path.realpath(__file__))
 # sys.path.insert(0, os.path.join(__dir, '../module'))
 
 
