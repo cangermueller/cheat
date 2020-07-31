28cf18cc2320422c260b920138249af66bfcec0b
Christof Angermueller <cangermueller@gmail.com>
2020-07-31 12:19:29 -0700

    Update cheat sheets

--- vi/install.sh
+++ vi/install.sh
@@ -1,17 +1,32 @@
 #!/usr/bin/env bash
 
+# 200324
 ./configure \
     --with-features=huge \
-    --enable-perlinterp \
     --enable-python3interp \
     --enable-cscope \
     --enable-gui=auto \
     --enable-gtk2-check \
     --enable-gnome-check \
     --enable-multibyte \
-    --with-x \
-    --with-python3-config-dir=/usr/local/google/home/christofa/opt/stow/anaconda3/pkgs/python-3.7.0-hc3d631a_0/lib/python3.7/config-3.7m-x86_64-linux-gnu \
+    --with-python3-config-dir=/usr/local/google/home/christofa/opt/stow/anaconda3/pkgs/python-3.7.6-h0371630_2/lib/python3.7/config-3.7m-x86_64-linux-gnu \
     --enable-fontset \
     --enable-largefile \
     --enable-fail-if-missing \
     --prefix=$HOME/opt/stow/vim81
+
+# ./configure \
+#     --with-features=huge \
+#     --enable-perlinterp \
+#     --enable-python3interp \
+#     --enable-cscope \
+#     --enable-gui=auto \
+#     --enable-gtk2-check \
+#     --enable-gnome-check \
+#     --enable-multibyte \
+#     --with-x \
+#     --with-python3-config-dir=/usr/local/google/home/christofa/opt/stow/anaconda3/pkgs/python-3.7.0-hc3d631a_0/lib/python3.7/config-3.7m-x86_64-linux-gnu \
+#     --enable-fontset \
+#     --enable-largefile \
+#     --enable-fail-if-missing \
+#     --prefix=$HOME/opt/stow/vim81
