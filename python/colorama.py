2e0d327f7ac57b38b2623dbc48acb8a2ddb15b25
Christof Angermueller <cangermueller@gmail.com>
2023-11-11 19:07:33 -0800

    Update configs

--- python/colorama.py
+++ python/colorama.py
@@ -1,33 +1,33 @@
 #!/usr/bin/env python
 
-from colorama import Fore, Back, Style, init
+import colorama
 import sys
 
-init(strip=not sys.stdout.isatty(), autoreset=True)
+# colorama.init(strip=not sys.stdout.isatty(), autoreset=True)
 # autoreset=True: change to default after one command
 # strip=True: deactivate coloring
 
-print(Fore.BLUE + 'Blue foreground')
+print(colorama.Fore.BLUE + 'Blue foreground')
 print('Normal, since autoreset')
-print(Back.GREEN + 'Green background')
-print(Style.BRIGHT + 'bright text')
-print(Style.DIM + 'dim text')
-print(Fore.RESET + Back.RESET + Style.RESET_ALL + 'Normal text')
+print(colorama.Back.GREEN + 'Green background')
+print(colorama.Style.BRIGHT + 'bright text')
+print(colorama.Style.DIM + 'dim text')
+print(colorama.Fore.RESET + colorama.Back.RESET + colorama.Style.RESET_ALL + 'Normal text')
 
 fores = 'BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET'.split(', ')
 s = ''
 for fore in fores:
-    s += eval('Fore.{fore} + "{fore}"'.format(fore=fore)) + ' '
+    s += eval('colorama.Fore.{fore} + "{fore}"'.format(fore=fore)) + ' '
 print(s)
 
 values = 'BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, RESET'.split(', ')
 s = ''
 for value in values:
-    s += eval('Back.{0} + "{0}"'.format(value)) + ' '
+    s += eval('colorama.Back.{0} + "{0}"'.format(value)) + ' '
 print(s)
 
 values = 'DIM, NORMAL, BRIGHT, RESET_ALL'.split(', ')
 s = ''
 for value in values:
-    s += eval('Style.{0} + "{0}"'.format(value)) + ' '
+    s += eval('colorama.Style.{0} + "{0}"'.format(value)) + ' '
 print(s)
