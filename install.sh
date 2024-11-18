abb0c2f5439debfc414a539c84806da0ff8613ec
Christof Angermueller <cangermueller@gmail.com>
2024-11-17 20:42:54 -0800

    Update cheat sheets

--- install.sh
+++ install.sh
@@ -1,5 +1,24 @@
 # Installation notes
 
+# ~/.bash_profile
+# https://superuser.com/questions/544989/does-tmux-sort-the-path-variable
+# if [ -f /etc/profile ]; then
+#     PATH=""
+#         source /etc/profile
+#         fi
+#
+#         export BASH_SILENCE_DEPRECATION_WARNING=1
+#         source ~/.bashrc
+#
+
+# Dotdrop
+cd ~/etc/dotfiles
+# use -p to use other profile (does not work? nothing to install)
+.dotdrop/dotdrop.sh install --cfg $(pwd)/config.yaml
+
+# iterm
+Import preferences: General -> Settings
+
 ## Installing tmux-mem-cpu-load
 cd ~/.tmux/plugins/tmux-mem-cpu-load
 cmake .
